9a0f85a54d394145e186020beac49f05a6fb4e85
aayush-mhrzn7 <aayushmaharjan497@gmail.com>
2026-02-25 16:26:25 +0545

    chore: react scan

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Manrope } from "next/font/google";
 import { AppShell } from "@/components/layout/AppShell";
 
 import "./globals.css";
+import Script from "next/script";
+import Head from "next/head";
 
 const manrope = Manrope({
   variable: "--font-sans",
@@ -25,6 +27,12 @@ export default function RootLayout({
       <body
         className={`${manrope.variable} font-sans antialiased bg-background text-foreground`}
       >
+        {process.env.NODE_ENV === "development" && (
+          <script
+            src="https://unpkg.com/react-scan/dist/auto.global.js"
+            crossOrigin="anonymous"
+          />
+        )}
         <AppShell>{children}</AppShell>
       </body>
     </html>
